5a25af321fd8518efdaa505c73da7f229ca72cbb
simplay <silent.simplay@gmail.com>
2016-05-03 14:14:20 +0200

    Allow to normalizing a consecutive list of files

--- renamer.rb
+++ renamer.rb
@@ -3,18 +3,29 @@ require 'pry'
 files_to_rename_path = ARGV[0]
 association_file_path = ARGV[1]
 
-counter = 1
-fname_pairs = {}
-File.open(association_file_path, 'r') do |file|
-  while line = file.gets
-    next if line.nil?
-    fname = line.strip.split(" ").last.split("/").last
-    fname_pairs[counter] = fname
-    counter = counter + 1
+unless association_file_path.nil?
+  counter = 1
+  fname_pairs = {}
+  File.open(association_file_path, 'r') do |file|
+    while line = file.gets
+      next if line.nil?
+      fname = line.strip.split(" ").last.split("/").last
+      fname_pairs[counter] = fname
+      counter = counter + 1
+    end
+  end
+
+  fname_pairs.each do |idx, fname_path|
+    binding.pry
+    ext = "." + fname_path.split(".").last
+    File.rename(files_to_rename_path + fname_path, files_to_rename_path + idx.to_s + ext)
+  end
+
+else
+  fname_pairs = Dir["#{files_to_rename_path}*.*"]
+  fname_pairs.each_with_index do |fname_path, idx|
+    ext = "." + fname_path.split(".").last
+    File.rename(fname_path, files_to_rename_path + (idx+1).to_s + ext)
   end
 end
 
-fname_pairs.each do |idx, fname_path|
-  ext = "." + fname_path.split(".").last
-  File.rename(files_to_rename_path + fname_path, files_to_rename_path + idx.to_s + ext)
-end
